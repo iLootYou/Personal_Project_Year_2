9e14ac0229819d8e1b8503fc05984eda91c50b70
iLootYou <leon.kremer@student.nhlstenden.com>
2025-09-20 18:38:59 +0200

    Beginning of the team selector made

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests 
 import os
+import pandas as pd
 
 url = "https://www.football-data.co.uk/mmz4281/2526/E0.csv"
 response = requests.get(url)
@@ -15,3 +16,15 @@ if response.status_code == 200:
     print(f"Download successful to {file_path}")
 else:
     print("Failed to download file")
+
+print("To predict the winning team please enter the home team and away team")
+home_team = input("Enter the home team: ").strip().title()
+away_team = input("Enter the away team: ").strip().title()
+
+df = pd.read_csv(file_path)
+
+home_away_condition = (df["HomeTeam"] == home_team) & (df["AwayTeam"] == away_team)
+if home_away_condition.any():
+    print("yes")
+else:
+    print("The filled in teams have yet to play eachother or are invalid")

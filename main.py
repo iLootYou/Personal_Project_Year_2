e961c997dee24d665a9a956ca71b8fa7ddb3508a
iLootYou <leon.kremer@student.nhlstenden.com>
2025-09-20 17:57:18 +0200

    Updated version of updating data file

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests 
+import os
 
 url = "https://www.football-data.co.uk/mmz4281/2526/E0.csv"
 response = requests.get(url)
+folder = "data/"
+file = "Prem25-26.csv"
+
+# Full path to where the file will be saved
+file_path = os.path.join(folder, file)
 
 if response.status_code == 200:
-    with open("Prem25-26.csv", "wb") as f:
+    with open(file_path, "wb") as f:
         f.write(response.content)
     print("Download successful")
 else:

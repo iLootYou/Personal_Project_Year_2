a63cfc83608aeeb99229bb850f14d2df782885ff
iLootYou <leon.kremer@student.nhlstenden.com>
2025-09-24 21:30:23 +0200

    added the head2head function

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ print("To predict the winning team please enter the home team and away team")
 home_team = input("Enter the home team: ").strip().title()
 away_team = input("Enter the away team: ").strip().title()
 
+
 def current_season_update():
     # "wb" for write mode and binary mode so we can write raw bytes which is essential when downloading
     # from web request
@@ -26,6 +27,7 @@ def current_season_update():
     else:
         print("Failed to download file")
 
+
 def outcome():
     df = pd.read_csv(file_path)
 
@@ -34,12 +36,43 @@ def outcome():
     away_team_exists = (away_team in df["HomeTeam"].values) or (away_team in df["AwayTeam"].values)
 
     if home_team_exists and away_team_exists:
-        # We have to look the last 5 matches played (win, tie, loss), match history between the two clubs
-        last_5_matches()
+        # Look the last 5 matches played (win, tie, loss), match history between the two clubs
+        #last_5_matches()
+        head2head()
 
         # Look at who is playing at home, and if they have been winning or losing their home matches
     else:
         print("The filled in teams are invalid")
+
+
+def head2head():
+    matches = []
+
+    # The '*' is to get all the data files that end in .csv so we dont have to specify
+    for file in glob.glob(f"{folder}/*.csv"):
+        # Encoding 'ISO-8859-1' because the files werent in UTF-8 which is the standard for Pandas
+        df = pd.read_csv(file, on_bad_lines='skip', encoding='ISO-8859-1')
+
+        # Append the matches to the array
+        matches.append(df[
+            ((df["HomeTeam"] == home_team) | (df["AwayTeam"] == home_team)) & 
+            ((df["HomeTeam"] == away_team) | (df["AwayTeam"] == away_team))
+            ])
+    
+    # Merge the dataframes
+    all_matches = pd.concat(matches)
+
+    # Coerce to convert invalid dates to NaT instead of errors, formatted the date so it wont parse each element individually
+    all_matches["Date"] = pd.to_datetime(all_matches["Date"], dayfirst=True, errors='coerce')
+
+    # Sort by the date and get the last 5 
+    last_5 = all_matches.sort_values(by="Date").tail(5)
+  
+    # So we can print out the columns we need to see
+    subset = ['Date', 'HomeTeam', 'AwayTeam']
+
+    print(last_5[subset])
+
 
 def last_5_matches():
     home_matches = []
@@ -53,7 +86,8 @@ def last_5_matches():
         # Append the matches to the array where the team is in either the home or away column
         home_matches.append(df[(df["HomeTeam"] == home_team) | (df["AwayTeam"] == home_team)])
         away_matches.append(df[(df["HomeTeam"] == away_team) | (df["AwayTeam"] == away_team)])
-
+    
+    # Merge the dataframes
     all_home_matches = pd.concat(home_matches)
     all_away_matches = pd.concat(away_matches)
 
@@ -72,9 +106,11 @@ def last_5_matches():
     print()
     print(last_5_away[subset])
 
+
 def main():
     #current_season_update()
     last_5_matches()
+
 
 if __name__ == "__main__":
     main()

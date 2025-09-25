40b001c8c573d8d92b3f08acb32f36df2dba9a56
iLootYou <leon.kremer@student.nhlstenden.com>
2025-09-25 11:49:56 +0200

    added the home_matches function to see the outcome of the last 5 home games

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def outcome():
         head2head()
 
         # Look at who is playing at home, and if they have been winning or losing their home matches
-
+        home_matches()
     else:
         print("The filled in teams are invalid")
 
@@ -74,6 +74,32 @@ def head2head():
     print(last_5[subset])
 
 
+def home_matches():
+    matches = []
+
+    # The '*' is to get all the data files that end in .csv so we dont have to specify
+    for file in glob.glob(f"{folder}/*.csv"):
+        # Encoding 'ISO-8859-1' because the files werent in UTF-8 which is the standard for Pandas
+        df = pd.read_csv(file, on_bad_lines='skip', encoding='ISO-8859-1')
+
+        # Append the matches to the array where the home team is in the column HomeTeam
+        matches.append(df[df["HomeTeam"] == home_team])
+
+    # Merge the dataframes
+    all_home_matches = pd.concat(matches)
+
+    # Coerce to convert invalid dates to NaT instead of errors, dayfirst because i cant seem to get the format
+    all_home_matches["Date"] = pd.to_datetime(all_home_matches["Date"], dayfirst=True, errors='coerce')
+
+    # Sort by the date and get the last 5 
+    last_5 = all_home_matches.sort_values(by="Date").tail(5)
+
+        # So we can print out the columns we need to see
+    subset = ['Date', 'HomeTeam','AwayTeam','FTR']
+        
+    print(last_5[subset])
+
+
 def last_5_matches():
     home_matches = []
     away_matches = []
@@ -91,7 +117,7 @@ def last_5_matches():
     all_home_matches = pd.concat(home_matches)
     all_away_matches = pd.concat(away_matches)
 
-    # Coerce to convert invalid dates to NaT instead of errors, formatted the date so it wont parse each element individually
+    # Coerce to convert invalid dates to NaT instead of errors, dayfirst because i cant seem to get the format
     all_home_matches["Date"] = pd.to_datetime(all_home_matches["Date"], dayfirst=True, errors='coerce')
     all_away_matches["Date"] = pd.to_datetime(all_away_matches["Date"], dayfirst=True, errors='coerce')
 
